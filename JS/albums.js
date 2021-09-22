1e05b37bb3b87e60f020ea3c4eaead642a5bb745
KatherinWilches <katherinwilches23@gmail.com>
2021-09-22 13:34:53 -0500

    segundo commit

---
+++ JS/albums.js
@@ -0,0 +1,34 @@
+const album6 = () => {
+    /*let nombre = alert("Plastic Heart");*/
+    alert ("Plastic Heart");
+}
+
+
+const album5 = () => {
+    /*let nombre = alert("She Is Coming");*/
+    alert("She Is Coming");
+}
+
+
+const album4 = () => {
+    /*let nombre = alert("Yunger Now");*/
+    alert("Yunger Now");
+}
+
+
+const album3 = () => {
+    /*let nombre = alert("Miley Cyrus & Her Dead Petz");*/
+    alert("Miley Cyrus & Her Dead Petz");
+}
+
+
+const album2 = () => {
+    /*let nombre = alert("Bangerz");*/
+    alert("Bangerz");
+}
+
+
+const album1 = () => {
+    /*let nombre = alert("Can´t Be Tamed");*/
+    alert("Can´t Be Tamed");
+}
